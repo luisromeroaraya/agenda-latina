8206b8b02e50b0518ff4b1ed9d07180f992fa100
richardsgab <richardsmariagabriela@gmail.com>
2021-06-02 16:22:14 +0200

    Install Laravel in php8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,6 @@
 
         <title>Laravel</title>
 
-        <!-- TailwindCSS -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
