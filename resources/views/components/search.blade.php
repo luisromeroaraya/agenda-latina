42abf0ee9a04a872dc3ab3d6aa97f0680909e1b2
richardsgab <richardsmariagabriela@gmail.com>
2021-06-11 12:07:03 +0200

    Search filters added

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -2,7 +2,7 @@
     <form action="{{ route('events.search') }}" method="POST">
         @csrf
         <div class="bg-white flex items-center rounded-full shadow-xl">
-            <input class="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" name="search" id="search" type="text" placeholder="Buscar" value="{{ old('search') }}">
+            <input class="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" name="search" id="search" type="text" placeholder="Buscar" value="{{ request('search') }}">
 
             <div class="p-4">
                 <button class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
