9323452658fa8cafb9b67515af6f7b793d714bb1
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-06-22 13:57:20 +0200

    Add username field

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
 
         $user = User::create([
             'name' => $request->name,
-            'name' => $request->username,
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
